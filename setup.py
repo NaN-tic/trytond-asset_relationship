4c83fec7e7a51c203d610a618b1c0aa1747f6701
Raimon Esteve <raimonesteve@gmail.com>
2025-05-20 12:54:07 +0200

    FIX Removing deprecated args from setup

--- setup.py
+++ setup.py
@@ -9,9 +9,7 @@ from configparser import ConfigParser
 
 MODULE = 'asset_relationship'
 PREFIX = 'nantic'
-MODULE2PREFIX = {
-    'asset': 'nantic',
-    }
+MODULE2PREFIX = {}
 
 
 def read(fname):
@@ -48,19 +46,18 @@ for dep in info.get('depends', []):
         requires.append(get_require_version('%s_%s' % (prefix, dep)))
 requires.append(get_require_version('trytond'))
 
-tests_require = []
+tests_require = [
+    get_require_version('proteus'),
+]
+
 series = '%s.%s' % (major_version, minor_version)
 if minor_version % 2:
     branch = 'default'
 else:
     branch = series
-dependency_links = [
-    ('hg+https://hg@bitbucket.org/nantic/trytond-asset@%(branch)s'
-        '#egg=nantic_asset-%(series)s' % {
-            'branch': branch,
-            'series': series,
-            }),
-    ]
+
+dependency_links = []
+
 if minor_version % 2:
     # Add development index for testing with proteus
     dependency_links.append('https://trydevpi.tryton.org/')
@@ -70,6 +67,7 @@ setup(name='%s_%s' % (PREFIX, MODULE),
     description='',
     long_description=read('README'),
     author='NaN·tic',
+    author_email='info@nan-tic.com',
     url='http://www.nan-tic.com/',
     download_url="https://bitbucket.org/nantic/trytond-%s" % MODULE,
     package_dir={'trytond.modules.%s' % MODULE: '.'},
@@ -79,7 +77,7 @@ setup(name='%s_%s' % (PREFIX, MODULE),
         ],
     package_data={
         'trytond.modules.%s' % MODULE: (info.get('xml', [])
-            + ['tryton.cfg', 'view/*.xml', 'locale/*.po', 'tests/*.rst']),
+            + ['tryton.cfg', 'locale/*.po', 'tests/*.rst']),
         },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
